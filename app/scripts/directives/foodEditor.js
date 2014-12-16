c3073e7165948f996c726c60ac97b9d5a06258cf
joengenduvel <robby.decosemaeker@telenet.be>
2014-12-16 06:56:06 +0100

    added directive to edit food options

--- app/scripts/directives/foodEditor.js
+++ app/scripts/directives/foodEditor.js
@@ -1,9 +1,19 @@
 'use strict';
 
-angular.module('foodSupportApp').directive('foodEditor', function(){
+angular.module('foodSupportApp').directive('foodEditor', function () {
     return {
         restrict: 'A',
         templateUrl: 'views/templates/foodEditor.html',
-        scope: { foodEditorItem: '=item'}
+        scope: {
+            foodEditorItem: '=item',
+            newOptionValue: '='
+        },
+        link: function (scope) {
+            scope.addOption = function () {
+                scope.foodEditorItem.options.push(scope.newOption);
+                scope.newOption = {};
+
+            };
+        }
     };
 });
